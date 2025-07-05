63a6c3203d5b6ee1cbd71de20b1246ef1621408f
Ehsan Daneshgar <82010319+EhsanDaneshgar@users.noreply.github.com>
2025-07-04 20:55:48 -0600

    Update basic-select-where.sql

--- SQL Practice Portfolio/1_basics_select_where/basic-select-where.sql
+++ SQL Practice Portfolio/1_basics_select_where/basic-select-where.sql
@@ -3,6 +3,14 @@ use [CPSY205];
 --Selects the first 5 rows from the Employees table in the CPSY205 database
 SELECT  top 5 * FROM [dbo].[Employees];
 
+-- Retrieves the first 50 records from the Employees table,
+SELECT TOP (50) [EmployeeID],
+       [Age],
+       [Department],
+       [Compensation],
+       [Hire Date]
+FROM [CPSY205].[dbo].[Employees];
+
 --Retrieves the names of all columns in the Employees table (metadata query)
 SELECT COLUMN_NAME
 FROM INFORMATION_SCHEMA.COLUMNS
