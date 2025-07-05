2068342f105df3105cd6aad4dda430723b0c3e16
Ehsan Daneshgar <82010319+EhsanDaneshgar@users.noreply.github.com>
2025-07-04 20:08:30 -0600

    Add files via upload

---
+++ SQL Practice Portfolio/1_basics_select_where/basic-select-where.sql
@@ -0,0 +1,14 @@
+use [CPSY205];
+-- -- Selects the first 5 rows from the Employees table in the CPSY205 database
+SELECT  top 5 * FROM [dbo].[Employees];
+
+---- Retrieves the names of all columns in the Employees table (metadata query)
+SELECT COLUMN_NAME
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Employees';
+
+
+--all rows where the Department is either HR, IT, or Sales.
+SELECT *
+FROM [dbo].[Employees]
+WHERE [Department] IN ('IT' , 'Sales');
