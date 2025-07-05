4593e63dde0f30741b04b0216280e7242c202503
Ehsan Daneshgar <82010319+EhsanDaneshgar@users.noreply.github.com>
2025-07-04 20:34:26 -0600

    Add files via upload

---
+++ basic-select-where.sql
@@ -0,0 +1,19 @@
+use [CPSY205];
+
+--Selects the first 5 rows from the Employees table in the CPSY205 database
+SELECT  top 5 * FROM [dbo].[Employees];
+
+--Retrieves the names of all columns in the Employees table (metadata query)
+SELECT COLUMN_NAME
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Employees';
+
+
+--all rows where the Department is either HR, IT, or Sales.
+SELECT *
+FROM [dbo].[Employees]
+WHERE [Department] IN ('IT' , 'Sales');
+
+-- Counts how many records exist for each EmployeeID in the Employees table.
+SELECT [EmployeeID], COUNT(*) AS CountPerID FROM[dbo].[Employees]
+GROUP BY [EmployeeID];
