10c71d877cdf6f3d4fc6900082311a233ffd250b
Ehsan Daneshgar <82010319+EhsanDaneshgar@users.noreply.github.com>
2025-07-05 13:06:23 -0600

    Add files via upload

---
+++ SQL Practice Portfolio/7_ddl_create_alter/ddl_create_alter.sql
@@ -0,0 +1,21 @@
+-- Switches to the West_OIlfield database to execute the following table creation.
+USE [West_OIlfield];
+
+-- Creates a table named 'wells' with two columns:
+-- well_ID as the primary key and well_name as a variable-length string (up to 55 characters).
+create table wells (
+well_ID int PRIMARY KEY,
+well_name varchar (55)
+);
+
+-- Switches to the CPSY205 database to create or modify tables.
+USE CPSY205;
+
+-- Creates a new table named 'Customers' with a single column 'CustomerID' of type INT.
+CREATE TABLE dbo.Customers(
+CustomerID INT);
+
+ALTER TABLE [dbo].[Customers]
+ADD FirstName NVARCHAR (50),
+	LastName NVARCHAR (50),
+	Email NVARCHAR(100);
